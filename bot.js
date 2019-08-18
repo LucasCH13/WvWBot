0e51648f5cec8cec397cc9706d18f32bfa91e7ea
Lucas <lucas.charrier13@gmail.com>
2019-08-18 11:09:13 +0200

    clean

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-
 // modules
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
@@ -8,7 +7,6 @@ const config = require("./config.json");
 // Create an instance of a Discord client
 const client = new Discord.Client();
   
-
 /*
  * The ready event is vital, it means that only after this will your bot start reacting to information
  * received from Discord
@@ -21,20 +19,15 @@ client.on('ready', () => {
 client.on('message', message => {
   if(message.content.startsWith('!kd'))
   {
-    
         const url = 'https://api.guildwars2.com/v2/wvw/matches/stats?world=2103'
-
 
         async function getKD() {
 
             const response = await fetch(url);
             const data = await response.json();
-
-            
-            const { kills, deaths, maps } = data;
+            const { kills, deaths} = data;
 
             //k/d overall
-
             const red = kills.red / deaths.red;
             const kd_red = red.toFixed(2);
 
@@ -50,10 +43,8 @@ client.on('message', message => {
    
         } 
 
-           getKD();
+        getKD();
    }
-
-
 });
 
 client.login(config.token);
